1a7e7dc2093def7d6a155e923bf5f886059e72af
syedirfan13 <syed.irfan.hasan13@gmail.com>
2022-09-14 18:15:38 +0530

    feat: notes

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import {
   CardContent,
   Card,
@@ -17,8 +18,43 @@ import {
   Button,
 } from '@mui/material';
 import Iconify from '../Iconify';
+import {
+  useGetCandidateNotesListQuery,
+  useGetNotesTypesQuery,
+  useAddCandidateNotesMutation,
+} from '../../redux/services/notes/NotesServices';
+import { showToast } from '../../utils/toast';
 
-const Notes = () => {
+const Notes = (props) => {
+  const { data: candidateNotesData, refetch } = useGetCandidateNotesListQuery(props.candidateId);
+  // const [addNotesData] = useAddCandidateNotesMutation();
+  const [addCandidateNotes, addCandidateNotesInfo] = useAddCandidateNotesMutation();
+
+  const [note, setNotes] = useState();
+  // const [value, setValue] = useState([]);
+
+  const notesChange = (e) => {
+    setNotes(e.target.value);
+    //  console.log(notes)
+  };
+  const addNotesHandler = () => {
+    addCandidateNotes({
+      candidate: props.candidateId,
+      note,
+      type: 1,
+    });
+  };
+  useEffect(() => {
+    if (addCandidateNotesInfo.isSuccess) {
+      showToast('success', 'Assesment Saved Sucessfully');
+      refetch();
+      // setAssesmentId(savedAssesmentRecord.id);
+      // addCandidateNotesInfo.reset();
+    }
+    if (addCandidateNotesInfo.isError) {
+      showToast('error', addCandidateNotesInfo.error.data.msg);
+    }
+  }, [addCandidateNotesInfo, refetch]);
   return (
     <>
       <Grid container>
@@ -34,10 +70,12 @@ const Notes = () => {
               fullWidth
               variant="outlined"
               size="small"
+              value={note}
+              onChange={notesChange}
             />
           </Grid>
           <Grid item md={2}>
-            <Button style={{ minWidth: 0 }} variant="contained">
+            <Button style={{ minWidth: 0 }} variant="contained" onClick={addNotesHandler}>
               <ListItemIcon style={{ color: '#fff', padding: '0px', minWidth: 0 }}>
                 <Iconify icon="akar-icons:plus" width={20} height={25} />
               </ListItemIcon>
@@ -50,14 +88,16 @@ const Notes = () => {
           <Typography variant="subtitle2" sx={{ mb: 1, ml: 1 }}>
             Notes
           </Typography>
-          <Card style={{ backgroundColor: '#5656561f' }}>
-            <CardContent>
-              <Typography variant="body2">
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
-                took a galley of type and scrambled it to make a
-              </Typography>
-            </CardContent>
-          </Card>
+          {candidateNotesData.notes &&
+            candidateNotesData.notes.map((item, i) => {
+              return (
+                <Card style={{ backgroundColor: '#5656561f' }}>
+                  <CardContent>
+                    <Typography variant="body2">{item.note}</Typography>
+                  </CardContent>
+                </Card>
+              );
+            })}
           <Grid container sx={{ mt: 1, ml: 1 }}>
             <Grid item md={8}>
               <Typography color="silver" style={{ fontSize: '12px' }}>
