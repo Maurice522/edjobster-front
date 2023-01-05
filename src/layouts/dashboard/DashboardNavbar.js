f878a96e6b61b46a5b3ec80ec240094b943ce54c
Kundan1804 <kundanagrawal18@gmail.com>
2023-01-05 22:47:19 +0530

    register page Updated

--- src/layouts/dashboard/DashboardNavbar.js
+++ src/layouts/dashboard/DashboardNavbar.js
@@ -22,7 +22,8 @@ const RootStyle = styled(AppBar)(({ theme }) => ({
   WebkitBackdropFilter: 'blur(6px)', // Fix on Mobile
   backgroundColor: alpha(theme.palette.background.default, 0.72),
   [theme.breakpoints.up('lg')]: {
-    width: `calc(100% - ${DRAWER_WIDTH + 1}px)`,
+    // width: `calc(100% - ${DRAWER_WIDTH + 1}px)`,
+    width:`calc(100% )`,
   },
 }));
 
