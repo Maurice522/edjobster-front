c1242e289a5d54cb971242d575206b89168781d3
notRyuk <amadeusrobot.notryuk@gmail.com>
2023-01-17 19:50:26 +0530

    Updated the main login conditions

--- src/App.js
+++ src/App.js
@@ -18,14 +18,13 @@ export default function App() {
   const navigate = useNavigate();
   const auth = JSON.parse(localStorage.getItem("globalUser"))
   useEffect(() => {
-    // if(!auth && !auth.account) {
-    //   navigate("/register")
-    // }
-    // else {
-    if(!auth && !auth.account)
+    if(!auth && !auth.account) {
       navigate("/login")
+    }
+    // else {
+    //   navigate("/login")
     // }
-  }, [navigate]);
+  }, []);
 
   return (
     <ThemeProvider>
