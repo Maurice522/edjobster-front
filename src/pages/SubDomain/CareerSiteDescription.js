c68d68d46125bdc3175868c173abaa7a03bf91e9
Kundan1804 <kundanagrawal18@gmail.com>
2023-01-25 17:45:03 +0530

    Career site frames ready

--- src/pages/SubDomain/CareerSiteDescription.js
+++ src/pages/SubDomain/CareerSiteDescription.js
@@ -16,6 +16,9 @@ import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
+import WorkIcon from '@mui/icons-material/Work';
+import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import FileUpload from 'react-material-file-upload';
 
 function CareerSiteDescription() {
@@ -29,12 +32,12 @@ function CareerSiteDescription() {
     return (
         <div>
             <h1 className='InstituteTitle'>SRM Institute Of Technology (Institute-Title)</h1>
-            <hr style={{ width: '70%', color: 'grey', margin: '5% auto', justifyContent: "center",marginBottom:"2%" }} />
+            <hr style={{ width: '70%', color: 'grey', margin: '5% auto', justifyContent: "center", marginBottom: "2%" }} />
             <TabContext value={value}>
-                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <TabList onChange={handleChange} aria-label="lab API tabs example">
-                        <Tab label="Item One" value="1" sx={{width:"100%"}}/>
-                        <Tab label="Item Three" value="2" sx={{width:"100%"}}/>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider',display:"flex",justifyContent:"center" }}>
+                    <TabList onChange={handleChange} aria-label="lab API tabs example" sx={{display:"flex",justifyContent:"center"}}>
+                        <Tab label="About" value="1" sx={{ width: "10%" }} />
+                        <Tab label="Job" value="2" sx={{ width: "10%" }} />
                     </TabList>
                 </Box>
                 <TabPanel value="1">
@@ -60,29 +63,57 @@ function CareerSiteDescription() {
                         </div>
                     </Card>
                 </TabPanel>
-                <TabPanel value="2">
-                    Item Three
+                <TabPanel value="2" sx={{
+                    marginLeft:"20%"
+                }}>
+                    <Card sx={{
+                        borderRadius: "14px",
+                        boxSizing: "border-box 1px solid #eaf1f5",
+                        width:"60%"
+                    }}>
+                        <div className='JobInnerCard'>
+                            <Stack sx={{
+                                display: "flex",
+                                flexDirection: "row",
+                                gap: "2%"
+
+                             }}>
+                                <div className='JobCardImgDiv'>
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRvPseQ_eXBy0JHhwi0Os_KRiq9mASdmH6EGWF_
+                                    e34FegX6NUDtfWh_f-jUq7Cjt5gHVE&usqp=CAU" alt="InstitutePhoto"
+                                    />
+                                </div>
+                                <div>
+                                    <h2>Job Title</h2>
+                                    <h4>Institute Name</h4>
+                                </div>
+                            </Stack>
+                            <Stack sx={{
+                                display: "flex",
+                                flexDirection: "row",
+                                gap: "4%",
+                                marginTop:"2%",
+                            }}>
+                                <div style={{alignItems:"center",display:"flex",flexDirection:"row"}}>
+                                    <WorkIcon color='disabled' />
+                                    <p>Expirience</p>
+                                </div>
+                                <div style={{alignItems:"center",display:"flex",flexDirection:"row"}}>
+                                <CurrencyRupeeIcon color='disabled'sx={{marginTop:"2%",paddingTop:"2%"}}/>
+                                    <p>Salary</p>
+                                </div>
+                                <div style={{alignItems:"center",display:"flex",flexDirection:"row"}}>
+                                <LocationOnIcon color='disabled'/>
+                                    <p>Location</p>
+                                </div>
+                            </Stack>
+                            <Typography variant="body1" gutterBottom sx={{marginTop:"2%"}}>
+                               Job Description
+                            </Typography>
+                        </div>
+                    </Card>
                 </TabPanel>
             </TabContext>
-            {/* <div className='InstitutePhotoAddress'>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRvPseQ_eXBy0JHhwi0Os_KRiq9mASdmH6EGWF_
-                    e34FegX6NUDtfWh_f-jUq7Cjt5gHVE&usqp=CAU" alt="InstitutePhoto"
-                />
-                <div className='InstituteAddress'>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
-                    text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
-                    survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. (landmark, city, state, country)
-                </div>
-            </div>
-
-            <div className='InstituteDescription'>
-                <p>(Description)</p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
-                five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
-                release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including
-                versions of Lorem Ipsum.
-            </div> */}
         </div>
     )
 }
