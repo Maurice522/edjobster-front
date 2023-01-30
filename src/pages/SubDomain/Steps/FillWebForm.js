2883568b6b9ecaf521f7fcea3b0d08a152f36050
notRyuk <amadeusrobot.notryuk@gmail.com>
2023-01-30 12:49:41 +0530

    Passing the webform in url params

--- src/pages/SubDomain/Steps/FillWebForm.js
+++ src/pages/SubDomain/Steps/FillWebForm.js
@@ -1,22 +1,25 @@
+import { useParams } from 'react-router-dom';
 import React from 'react'
 import {
-    Card,
-    Stack,
-    Button,
-    Container,
-    Typography,
-    ListItemIcon,
-    Grid,
-    TextField,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-    DialogContent,
-    Box,
+  Card,
+  Stack,
+  Button,
+  Container,
+  Typography,
+  ListItemIcon,
+  Grid,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  DialogContent,
+  Box,
 } from '@mui/material';
 
 function FillWebForm() {
+  const params = useParams();
+  console.log(params)
   return (
     <div className='ApplicationSteps'>
 
